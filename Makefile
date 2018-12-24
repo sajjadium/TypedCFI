f2c6b049e2f1fb1a0b96417cabdb83b8abb1eca5
Sajjad Arshad <msarshadir@gmail.com>
2018-12-24 15:58:22 -0500

    adding more info to README

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-indirectmake: vulnerable_code.c
-	gcc -Wl,-z,relro,-z,now -fplugin=./rap_plugin.so -fplugin-arg-rap_plugin-typecheck=call,ret  -fplugin-arg-rap_plugin-report=func,fptr,abs vulnerable_code.c -m32 -fno-stack-protector -o vulnerable_code
+indirectmake: sample.c
+	gcc -Wl,-z,relro,-z,now -fplugin=./rap_plugin.so -fplugin-arg-rap_plugin-typecheck=call,ret  -fplugin-arg-rap_plugin-report=func,fptr,abs sample.c -m32 -fno-stack-protector -o sample
--- sample.c
+++ sample.c
@@ -2,27 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef void (*fptr) (void);
-int new_proc=0;
+typedef void (*fptr)(void);
+int new_proc = 0;
 char *p;
 
-void foo1 (void){
+void foo1 (void) {
 	printf("Indirect call of foo1\n");
 }
-void foo2 (void){
+
+void foo2 (void) {
 	printf("Indirect call of foo2\n");
 }
-int shell_execute(char *proc){
+
+int shell_execute(char *proc) {
 	system(proc);
 }
-int initial_proc(void){
-if (new_proc ==1)
-	shell_execute(p);
+
+int initial_proc(void) {
+	if (new_proc ==1)
+		shell_execute(p);
 }
-void spawn_process(void){
+
+void spawn_process(void) {
 	initial_proc();
 }
-void vulnfunc(char * input){
+
+void vulnfunc(char * input) {
 	fptr fIndirectCall;
 	char buf[20];
 
@@ -34,13 +39,13 @@ void vulnfunc(char * input){
 	strcpy(buf, input);
 	fIndirectCall();
 }
-int main(int argc, char *argv[])
-{
- 	if(argc <2)
- 	{
+
+int main(int argc, char *argv[]) {
+ 	if(argc <2) {
     	printf("Syntax: %s <input string>\n", argv[0]);
 	   	exit (0);
  	}
+ 	
 	vulnfunc(argv[1]);
 	exit(0);
 }
